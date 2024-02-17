e59b5bc11a576d291360c13e468e5f4d33910b22
shavkat <shavkatabdunasimov@gmail.com>
2024-02-17 17:40:21 +0500

    add lesson1:task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 8;
+int b = 11;
+int c = 7;
+int d = 9;
+int e = 13;
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
